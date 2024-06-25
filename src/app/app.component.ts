3d05ede49c2f71b7ba2158e5566b943eb77e9a8d
Aji Ramadhani Setianto <ajiramadhani56@gmail.com>
2024-06-25 16:43:19 +0700

    update home view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { register } from "swiper/element/bundle";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'angular-portofolio';
 }
+register()
